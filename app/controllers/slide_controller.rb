0fb5ff9cf0dcc9c72d5d26d3701205147f3524f8
Travis Valentine <tvalent2@gmualumni.org>
2014-01-01 10:41:24 -0500

    Fix values/bar position/value submitted

--- app/controllers/slide_controller.rb
+++ app/controllers/slide_controller.rb
@@ -1,4 +1,6 @@
 class SlideController < UIViewController
+  MIN = 0
+  MAX = 10
 
   def viewDidLoad
     super
@@ -21,7 +23,7 @@ class SlideController < UIViewController
       queue = NSOperationQueue.alloc.init
 
       device_motion_handler = lambda do |motion, error|
-        submit_position(motion, error)
+        reposition_bar_and_submit(motion, error)
       end
 
       @tilt_manager.deviceMotionUpdateInterval = 1.0/60.0
@@ -35,19 +37,23 @@ class SlideController < UIViewController
 
   end
 
-  def submit_position(motion, error)
+  def reposition_bar_and_submit(motion, error)
     Dispatch::Queue.main.sync do
 
       elapsed_time = motion.timestamp - @last_timestamp
 
       if (@last_timestamp > 0)
-        divider_number     = (motion.attitude.pitch * 10).round
-        label.text = "#{divider_number}"
-        new_y_origin       = bar_position(divider_number)
-        new_frame          = bar.frame
-        new_frame.origin.y = new_y_origin
-        bar.frame          = new_frame
+        pitch_value = (motion.attitude.pitch * 10).round
 
+        if pitch_value >= 10
+          divider_number = MAX
+        elsif pitch_value <= 0
+          divider_number = MIN
+        else
+          divider_number = pitch_value
+        end
+
+        reposition_bar(divider_number + 1)
         submit_feedback("#{divider_number}")
       end
 
@@ -64,11 +70,18 @@ class SlideController < UIViewController
     end
   end
 
+  def reposition_bar(divider_number)
+    new_y_origin       = bar_position(divider_number)
+    new_frame          = bar.frame
+    new_frame.origin.y = new_y_origin
+    bar.frame          = new_frame
+  end
+
   def submit_feedback(value)
     data = {
       'response[event_id]' => 1,
       'response[user_id]'  => 3,
-      'response[value]'    => value,
+      'response[value]'    => array[value.to_i][1],
       'user[api_token]'    => "1cfc0f51520db5a3f5dfebb8bd437618"
     }
     AFMotion::Client.shared.post("events/1/responses", data) do |result|
@@ -87,23 +100,21 @@ class SlideController < UIViewController
 
   def bar
     @bar ||= UIView.alloc.initWithFrame(CGRectZero).tap do |b|
-      b.frame = CGRect.new([0, bar_position(10)], [self.view.frame.size.width, 5])
+      b.frame = CGRect.new([0, bar_position(11)], [self.view.frame.size.width, 5])
       b.backgroundColor = UIColor.blackColor
     end
   end
 
   def build_dividers
-    array = (1..10).to_a
-
-    1.upto(10) do |n|
-      frame = CGRect.new([0, (n * divider_height) + 10], [self.view.frame.size.width, divider_height])
+    MIN.upto(MAX) do |n|
+      frame = CGRect.new([0, (n * divider_height) + height], [self.view.frame.size.width, divider_height])
       divider_view = UIView.alloc.initWithFrame(frame)
       divider_view.backgroundColor  = UIColor.colorWithRed(235.0/255, green:235.0/255, blue:235.0/255, alpha:1)
       divider_view.layer.borderWidth = 0.5
       divider_view.layer.borderColor = UIColor.lightGrayColor.CGColor
 
       divider_label           = UILabel.alloc.initWithFrame(frame)
-      divider_label.text      = "#{array[-n]}"
+      divider_label.text      = "#{array[n][1]}"
       divider_label.textColor = UIColor.darkGrayColor
       divider_label.font      = UIFont.systemFontOfSize(20)
       divider_label.sizeToFit
@@ -116,7 +127,7 @@ class SlideController < UIViewController
   end
 
   def divider_height
-    @divider_height ||= (self.view.frame.size.height - height) / 10
+    @divider_height ||= (self.view.frame.size.height - height) / (MAX + 1)
   end
 
   def height
@@ -127,8 +138,15 @@ class SlideController < UIViewController
   end
 
   def bar_position(divider_number)
-    divider = (divider_number * divider_height) + divider_height + 10
+    divider = (divider_number * divider_height) + height
     divider - (divider_height / 2)
+  end
+
+  def array
+    @array ||= [
+      [0, 10], [1, 9], [2, 8], [3, 7], [4, 6], [5, 5],
+      [6, 4], [7, 3], [8, 2], [9, 1], [10, 0]
+    ]
   end
 
 end
